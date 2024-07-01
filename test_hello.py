5fb38e068bae78691ef4e80809a3c83726c5f3f1
Lewis <dumesnil.hounkpevi@gmail.com>
2024-07-01 10:21:51 +0000

    more tests

--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,19 @@
-from hello import add
+from hello import add, subtract
 
 
-def test_add():
-    assert add(1, 2) == 3
+# def setup_function(function):
+#     print(f" Running Setup: {function.__name__}")
+#     function.x = 10
+
+
+# def teardown_function(function):
+#     print(f" Running Teardown: {function.__name__}")
+#     del function.x
+
+
+### Run to see failed test
+def test_hello_add():
+    assert add(10) == 11
+
+def test_hello_subtract():
+    assert subtract(10) == 9
